a761c9bad87116de05a21e11ea6928c6a8066b54
zeita101 <zeitajb1@gmail.com>
2020-10-06 21:04:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,28 @@
 // even numbers of an array
 // Now write a function that returns only the odds
 
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function filterEven(){
+// const evens = arr.filter(x => x % 2 === 0);
+//    return x % 2 === 0;
+//    console.log(evens);
+
+// }
+
+function filterEven(){
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let evens = arr.filter(x => x % 2 === 0);
+  console.log(evens);
+}
+filterEven();
+
+
+function filterOdd(){
+  let arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  let odds = arr1.filter(x => x % 3 === 0);
+  console.log(odds);
+}
+filterOdd();
 
 
 
@@ -13,28 +35,44 @@
 // by 6
 
 
+function filterSix(){
 
 
+let arr1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+let divBySix = arr1.filter(function(x) {
+     return x % 6 === 0;
+     })
+     console.log(divBySix);
+}
+filterSix();
 
 //Exercise #3
 // using reduce, add up all numbers in an array
 // ex: [10,12,20,50] => 92
 
 
+const cash = [29.76, 41.85, 46.5];
 
+const sum = cash.reduce((total, amount) => total + amount); 
+console.log(sum);
 
 
 //Exercise #4
 //Using Reduce, multiply all numbers in a given array
 //ex: multiply([1, 2, 3, 4, 5]) => 120
 
-
+const arr = [2, 4, 6, 8, 10];
+const product = arr.reduce((total, amount) => total * amount); console.log(product);
 
 
 //Exercise #5
 //using reduce, power up all numbers in a given array
 // ex:  squareNums([3, 2, 3]) => 729, since (3^2) = 9 and then (9 ^ 3) = 729
 
+
+const arr = [2, 4,];
+const product = arr.reduce((total, amount) => total **
+ amount); console.log(product); 
 
 
 
@@ -50,6 +88,10 @@ let users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resid
   { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor' }];
              
 
+let residents = users.filter(function (e) {
+    return e.role === 'Full Stack Resident';
+});
+console.log(residents);
 
 //Bonus
 //Using Reduce

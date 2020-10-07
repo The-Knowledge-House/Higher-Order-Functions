a7564d00fa32612167f780b991ede2e172df13b3
TKH Student 1 <tkh@TKHs-Air-2.home>
2020-10-06 21:26:16 -0400

    Jahaziel Israel

--- main.js
+++ main.js
@@ -3,8 +3,17 @@
 // even numbers of an array
 // Now write a function that returns only the odds
 
+let arr = [1, 2, 3, 4];
 
+const evenResult = arr.filter(function (element) {
+  return (element % 2 == 0)
+}) 
+console.log(evenResult)
 
+const oddResult = arr.filter(function (element) {
+  return (element % 2 !== 0)
+}) 
+console.log(oddResult)
 
 
 //Exercise #2
@@ -13,14 +22,23 @@
 // by 6
 
 
-
+let numbers = [24, 7, 6, 800, 66, 11, 99]
+const isDivisible = numbers.filter(function(divSix) {
+  return (divSix % 6 === 0)
+})
+console.log(isDivisible)
 
 
 //Exercise #3
 // using reduce, add up all numbers in an array
 // ex: [10,12,20,50] => 92
 
+const array1 = [10,12,20,50];
 
+const sum = array1.reduce(function(accumulator, value) {
+  return accumulator + value
+},)
+console.log(sum)
 
 
 
@@ -28,12 +46,26 @@
 //Using Reduce, multiply all numbers in a given array
 //ex: multiply([1, 2, 3, 4, 5]) => 120
 
+const multiplyArr = [1, 2, 3, 4, 5];
+
+const multiply = multiplyArr.reduce(function (accumulator, value){
+  return accumulator * value
+}, 1)
+console.log(multiply);
+
 
 
 
 //Exercise #5
 //using reduce, power up all numbers in a given array
 // ex:  squareNums([3, 2, 3]) => 729, since (3^2) = 9 and then (9 ^ 3) = 729
+
+const powerArr = [3, 2, 3];
+
+const power = powerArr.reduce(function (accumulator, value){
+  return Math.pow(accumulator, value)
+})
+console.log(power);
 
 
 
@@ -50,9 +82,21 @@ let users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resid
   { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor' }];
              
 
+let residents = users.filter(function (e) {
+  return e.role === 'Full Stack Resident';
+});
+
+console.log(residents);
+
+
+
 
 //Bonus
 //Using Reduce
 //add up all the ages in the array of objects
 // the ages in the array of objects below should equate to 78
 [{name: 'Josh', age: 24}, {name: 'Meghan', age: 34}, {name: 'Samantha', age: 20}]
+
+
+const sumAge = (array) =>
+  array.reduce((acc, cur) => acc + cur.age, 0);

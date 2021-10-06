6e3abfab0067dcbb3061b3c5a50fff7842a57fa5
KennyO20123 <82979970+KennyO20123@users.noreply.github.com>
2021-10-05 23:51:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,13 @@
 // even numbers of an array
 // Now write a function that returns only the odds
 
-
+let array1 = [1, 2, 3, 4, 5, 6, 7, 8];
+let result = array1.filter(function (value) {
+  if (value % 2 != 0) {
+    return value;
+  }
+});
+console.log(result);
 
 
 
@@ -12,7 +18,13 @@
 // of numbers and returns an array of only numbers that are divisible
 // by 6
 
-
+let array2 = [6, 2, 3, 4, 5, 6, 12, 18];
+let result2 = array2.filter(function (value2) {
+  if (value2 % 6 == 0) {
+    return value2;
+  }
+});
+console.log(result2);
 
 
 
@@ -20,22 +32,38 @@
 // using reduce, add up all numbers in an array
 // ex: [10,12,20,50] => 92
 
+let array3 = [10, 12, 20, 50];
 
-
+let result3 = array3.reduce(function (previousValue3, currentvalue3) {
+  return previousValue3 + currentvalue3;
+});
+console.log(result3);
 
 
 //Exercise #4
 //Using Reduce, multiply all numbers in a given array
 //ex: multiply([1, 2, 3, 4, 5]) => 120
 
+let array4 = [1, 2, 3, 4, 5];
 
+let result4 = array4.reduce(function (previousValue4, currentvalue4) {
+  return previousValue4 * currentvalue4;
+});
+console.log(result4);
 
 
 //Exercise #5
 //using reduce, power up all numbers in a given array
 // ex:  squareNums([3, 2, 3]) => 729, since (3^2) = 9 and then (9 ^ 3) = 729
 
+function squareNums (arr) {
+  let total = arr.reduce((acc, val) => {
+    return Math.pow(acc, val);
+  })
+  return total;
+}
 
+console.log(squareNums([3, 2, 3]));
 
 
 //Exercise #6
@@ -48,6 +76,10 @@ let users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resid
              { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
              { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
   { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor' }];
+
+let students = users.filter((user) => user.role === 'Full Stack Resident')
+  
+console.log(students)
              
 
 
@@ -56,3 +88,9 @@ let users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resid
 //add up all the ages in the array of objects
 // the ages in the array of objects below should equate to 78
 [{name: 'Josh', age: 24}, {name: 'Meghan', age: 34}, {name: 'Samantha', age: 20}]
+
+let people = [{ name: 'Josh', age: 24 }, { name: 'Meghan', age: 34 }, { name: 'Samantha', age: 20 }]
+
+const sumAge = (array) => array.reduce((acc, curr) => acc + curr.age, 0);
+
+console.log(sumAge(people))
